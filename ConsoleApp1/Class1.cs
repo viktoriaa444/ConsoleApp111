5983e7d66d45cad7a2fcd3ac91fe5a1ed4588813
viktoriaa444 <118815929+viktoriaa444@users.noreply.github.com>
2023-04-13 13:38:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GroupObject : DynamicObject
+    {
+        private readonly string name;
+        private readonly string shortName;
+        private readonly dynamic number;
+        private readonly dynamic yearAdmission;
+
+
+        public string Name { get { return name; } }
+        public string ShortName { get { return shortName; } }
+        public int Number { get { return number; } }
+        public int YearAdmission { get { return yearAdmission; } }
+
+
+        public GroupObject(string name, string shortName, int number, int yearAdmission)
+        {
+            this.name = name;
+            this.shortName = shortName;
+            this.number = number;
+            this.yearAdmission = yearAdmission;
+
+        }
+    }
+}
+
